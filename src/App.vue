0bf224c660cfa4b6b830a523539614f9e314378b
Thomas Klein <thomas.klein.1982@gmail.com>
2024-01-09 03:05:25 +0100

    assets werden nicht mehr doppelt gespeichert

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ export default{
   data(){
     return {
       screen: 'start',
-      version: 283,
+      version: 284,
       paused: false,
       printMode: false,
       current: {line: -1, name: null},
