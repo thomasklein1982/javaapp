385a6d768d891c7623745c328a02e005a7652874
Thomas Klein <thomas.klein.1982@gmail.com>
2024-01-19 00:19:52 +0100

    image editor pipette

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ export default{
   data(){
     return {
       screen: 'start',
-      version: 287,
+      version: 288,
       paused: false,
       printMode: false,
       current: {line: -1, name: null},
