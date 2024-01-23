647252cafba34b1478648093207837476dc28107
Thomas Klein <thomas.klein.1982@gmail.com>
2024-01-23 02:38:46 +0100

    v289

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ export default{
   data(){
     return {
       screen: 'start',
-      version: 288,
+      version: 289,
       paused: false,
       printMode: false,
       current: {line: -1, name: null},
