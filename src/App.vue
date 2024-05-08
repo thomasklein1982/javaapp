1d07b24b3c0ca5f9aa9747808665ca03d747376d
Thomas Klein <thomas.klein.1982@gmail.com>
2024-05-08 11:35:04 +0200

    gamepad fix

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ export default{
   data(){
     return {
       screen: 'start',
-      version: 325,
+      version: 326,
       paused: false,
       printMode: false,
       current: {line: -1, name: null, $scope: {local: null, main: null, that: null}},
