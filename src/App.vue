ef67c4f04d0d29e264cac81ca5b7093362b0392d
Thomas Klein <thomas.klein.1982@gmail.com>
2024-07-04 23:25:42 +0200

    v343

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@ export default{
   data(){
     return {
       screen: 'start',
-      version: 342,
+      version: 343,
       paused: false,
       printMode: false,
       current: {line: -1, step: 0, name: null, $scope: {local: null, main: null, that: null}},
