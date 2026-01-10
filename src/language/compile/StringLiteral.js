1a60b74775881302b8b47e7e8bec8d0a25a0be79
Thomas Klein <thomas.klein.1982@gmail.com>
2026-01-10 09:31:35 +0100

    check for invalid characters in stringliterals

--- src/language/compile/StringLiteral.js
+++ src/language/compile/StringLiteral.js
@@ -8,8 +8,14 @@ export function StringLiteral(node,source,scope){
   }
   let type=new Type(Java.datatypes.String,0);
   scope.addTypeAnnotation(node,type,false);
+  let code=source.getText(node);
+  try{
+    JSON.parse(code);
+  }catch(e){
+    throw source.createError("Der String enthält mindestens ein ungültiges Zeichen.\n"+e.message, node);
+  }
   return {
-    code: source.getText(node),
+    code: code,
     type
   };
 }
